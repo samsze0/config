fd310739fcffe956055f7eb4d5bd12e94519eb75
artizon-io <mingsum.sam@gmail.com>
2023-06-06 15:01:08 +0800

    powershell: add setup script

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -15,3 +15,22 @@ function wm {
 function kbd {
   ~/.config/kmonad/kmonad.exe "$([Environment]::GetEnvironmentVariable('userprofile'))/.config/kmonad/kmonad.kbd"
 }
+
+function setup {
+  param (
+    $PowerShell,
+    $WindowsTerminalPath,
+    $PowerToys
+  )
+  if ($PowerShell) {
+    sudo ln -s "$([Environment]::GetEnvironmentVariable('userprofile'))\.config\powershell\profile.ps1" "$([Environment]::GetEnvironmentVariable('userprofile'))\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+  }
+  if ($WindowsTerminalPath) {
+    Remove-Item "$WindowsTerminalPath\settings.json"
+    sudo ln -s "$([Environment]::GetEnvironmentVariable('userprofile'))\.config\windows-terminal\settings.json" "$WindowsTerminalPath\settings.json"
+  }
+  if ($PowerToys) {
+    sudo ln -s "$([Environment]::GetEnvironmentVariable('userprofile'))\.config\powertoys\keyboard-manager\settings.json" "$([Environment]::GetEnvironmentVariable('userprofile'))\AppData\Local\Microsoft\PowerToys\Keyboard Manager\settings.json"
+    sudo ln -s "$([Environment]::GetEnvironmentVariable('userprofile'))\.config\powertoys\keyboard-manager\default.json" "$([Environment]::GetEnvironmentVariable('userprofile'))\AppData\Local\Microsoft\PowerToys\Keyboard Manager\default.json"
+  }
+}

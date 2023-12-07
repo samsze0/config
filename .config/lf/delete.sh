104d853a0f747376dae55e9b9ee30a9115c515bd
Sam Sze <mingsum.sam@gmail.com>
2023-12-07 15:12:24 +0800

    lf trash

--- .config/lf/delete.sh
+++ .config/lf/delete.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-set -f
-for f in $@; do
-    if [ ! -e $f ]; then
-        echo "file not found: $f"
-        exit 1
-    fi
-    echo $f
+set -f # disable filename expansion
+
+declare -a files
+
+trash_mode=false
+
+while (("$#")); do
+	case "$1" in
+	--trash)
+		trash_mode=true
+		shift
+		;;
+	*)
+		files+=("$1")
+		shift
+		;;
+	esac
 done
+
+for f in "${files[@]}"; do
+	if [ ! -e $f ]; then
+		echo "file not found: $f"
+		exit 1
+	fi
+	echo $f
+done
+
 echo ""
-printf "delete? [y/n] "
-read ans
-[ $ans = "y" ] && rm -rf $@
+
+if $trash_mode; then
+	printf "Trash? [y/n] "
+	read ans
+	[ $ans = "y" ] && trash ${files[@]}
+else
+	printf "Delete? [y/n] "
+	read ans
+	[ $ans = "y" ] && rm -rf ${files[@]}
+fi

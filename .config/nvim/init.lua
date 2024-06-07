f7affda6b483b8e1efa1356c8ca946f1a9e54c40
Sam Sze <mingsum.sam@gmail.com>
2024-06-07 13:58:41 +0800

    (nvim): specify plugin versions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,15 +84,18 @@ require("lazy").setup({
   {
     "ibhagwan/fzf-lua",
     config = function() require("config.fzflua").setup() end,
+    commit = "d368f76b37448d31918c81f020b0c725781c8354",
   },
   {
     "zbirenbaum/copilot.lua",
     enabled = not vim.g.vi_mode,
     config = function() require("config.copilotlua") end,
+    commit = "f7612f5af4a7d7615babf43ab1e67a2d790c13a6",
   },
   {
     "norcalli/nvim-colorizer.lua",
     config = function() require("config.colorizer") end,
+    commit = "a065833f35a3a7cc3ef137ac88b5381da2ba302e",
   },
   {
     "neovim/nvim-lspconfig",
@@ -101,16 +104,19 @@ require("lazy").setup({
       "b0o/schemastore.nvim",
     },
     config = function() require("config.lspconfig") end,
+    tag = "v0.1.8",
   },
   {
     -- Setup neovim plugin/script dev env by configuring lua-language-server with lsp-config
     -- Also add type annotations to vim/neovim built-in functions and APIs
     "folke/neodev.nvim",
+    tag = "v3.0.0",
   },
   {
     -- Git status in sign column, git hunk preview/navigation, and line blame
     "lewis6991/gitsigns.nvim",
     config = function() require("config.gitsigns") end,
+    tag = "v0.8.1",
   },
   {
     "numToStr/Comment.nvim",
@@ -119,21 +125,25 @@ require("lazy").setup({
         mappings = false,
       })
     end,
+    tag = "v0.8.0",
   },
   {
     -- Formatters interface that calculates minimal diff
     "stevearc/conform.nvim",
     config = function() require("config.conform") end,
+    tag = "v5.8.0",
   },
   {
     -- Linters interface that reports to vim.diagnostic
     "mfussenegger/nvim-lint",
     config = function() require("config.nvimlint") end,
+    commit = "1a3a8d047bc01f1760ae4a0f5e80f111ea222e67",
   },
   {
     "hrsh7th/nvim-cmp",
     config = function() require("config.nvimcmp") end,
     dependencies = {
+      -- TODO: fix versions for completion sources
       "hrsh7th/cmp-nvim-lsp", -- source for lsp
       "hrsh7th/cmp-path", -- source for filesystem paths
       "hrsh7th/cmp-cmdline", -- source for vim command line
@@ -142,6 +152,7 @@ require("lazy").setup({
       "onsails/lspkind.nvim", -- add vscode-codicons to popup menu
       "L3MON4D3/LuaSnip", -- snippet
     },
+    commit = "5260e5e8ecadaf13e6b82cf867a909f54e15fd07",
   },
   {
     "dstein64/nvim-scrollview",
@@ -150,31 +161,35 @@ require("lazy").setup({
         floating_windows = true,
       })
     end,
+    commit = "9257c3f3ebf7608a8711caf44f878d87cd40395d",
   },
   {
     "nvim-treesitter/nvim-treesitter",
     config = function() require("treesitter") end,
+    commit = "v0.9.2",
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    enabled = false,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
+    enabled = false,
   },
   {
     "mfussenegger/nvim-dap",
-    enabled = false, -- TODO
     config = function() end,
+    enabled = false,
   },
   {
     "nvim-treesitter/playground",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
+    enabled = false,
   },
   { -- Required by fzf & lf
     "MunifTanjim/nui.nvim",
+    tag = "0.3.0",
   },
   utils_nvim_lazy_spec,
   {
@@ -214,7 +229,7 @@ require("lazy").setup({
     dependencies = {
       utils_nvim_lazy_spec,
     },
-  }
+  },
 })
 
 if vim.g.neovide then require("config.neovide") end

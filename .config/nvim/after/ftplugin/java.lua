252d7042a6789735f273d2a1e19754be32bd087e
Sam Sze <mingsum.sam@gmail.com>
2024-06-08 14:55:14 +0800

    (java): jdtls lombok support

--- .config/nvim/after/ftplugin/java.lua
+++ .config/nvim/after/ftplugin/java.lua
@@ -2,21 +2,27 @@
 
 local os_utils = require("utils.os")
 local lang_utils = require("utils.lang")
+local jdtls = require("jdtls")
 
 local jdtls_path = os.getenv("JDTLS_HOME")
 local jdtls_system_config_path = lang_utils.match(os_utils.OS, {
   ["Darwin"] = jdtls_path .. "/config_mac",
   ["Linux"] = jdtls_path .. "/config_linux",
 })
-local jdtls_equinox_launcher_version = "1.6.800.v20240513-1750" -- TODO
-local jdtls_equinox_launcher_path = jdtls_path
-  .. "/plugins/org.eclipse.equinox.launcher_"
-  .. jdtls_equinox_launcher_version
-  .. ".jar"
+local jdtls_equinox_launcher_path =
+  vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
 
 -- https://github.com/mfussenegger/nvim-jdtls?tab=readme-ov-file#data-directory-configuration
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = vim.fn.getcwd() .. "/.jdtls"
+
+local lombok_path = vim.fn.getcwd() .. "/.jdtls/lombok.jar"
+local lombok_exists = vim.fn.filereadable(lombok_path)
+if not lombok_exists then
+  vim.cmd(
+    "curl https://projectlombok.org/downloads/lombok.jar -o " .. lombok_path
+  )
+end
 
 if not jdtls_system_config_path then error("Unsupported OS " .. os_utils.OS) end
 
@@ -36,24 +42,37 @@ local config = {
     "java.base/java.util=ALL-UNNAMED",
     "--add-opens",
     "java.base/java.lang=ALL-UNNAMED",
+
     "-jar",
     jdtls_equinox_launcher_path,
+
     "-configuration",
     jdtls_system_config_path,
+
     -- See `data directory configuration` section in the README
     "-data",
     workspace_dir,
+
+    -- Lombok support
+    "-javaagent",
+    lombok_path,
   },
 
   -- vim.fs.root requires Neovim 0.10.
   -- If you're using an earlier version, use: require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-  root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
+  root_dir = vim.fs.root(
+    0,
+    { ".git", "mvnw", "gradlew", "build.gradle", "pom.xml" }
+  ),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   -- for a list of options
   settings = {
-    java = {},
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = jdtls.extendedClientCapabilities,
+    },
   },
 
   -- Language server `initializationOptions`
@@ -65,4 +84,4 @@ local config = {
   },
 }
 
-require("jdtls").start_or_attach(config)
+jdtls.start_or_attach(config)

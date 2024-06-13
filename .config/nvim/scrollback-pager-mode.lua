f0264cc3474b7f1620fd8c98b52500347789dddb
Sam Sze <mingsum.sam@gmail.com>
2024-06-13 09:42:26 +0800

    (kitty): kitty config update

--- .config/nvim/scrollback-pager-mode.lua
+++ .config/nvim/scrollback-pager-mode.lua
@@ -7,18 +7,23 @@ vim.wo.signcolumn = "no"
 
 vim.opt.laststatus = 0 -- hide statusline
 
-local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+local current_buf = vim.api.nvim_get_current_buf()
+
+local lines = vim.api.nvim_buf_get_lines(current_buf, 0, -1, false)
 while #lines > 0 and vim.trim(lines[#lines]) == "" do
   lines[#lines] = nil
 end
 
-local buf = vim.api.nvim_create_buf(false, true)
-local channel = vim.api.nvim_open_term(buf, {})
+local new_buf = vim.api.nvim_create_buf(false, true)
+local channel = vim.api.nvim_open_term(new_buf, {})
 
 vim.api.nvim_chan_send(channel, table.concat(lines, "\r\n"))
-vim.api.nvim_set_current_buf(buf)
+-- vim.fn.chanclose(channel)
+vim.api.nvim_set_current_buf(new_buf)
 
-vim.keymap.set("n", "q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
-vim.keymap.set("n", "<space>q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
+vim.api.nvim_buf_delete(current_buf, { force = true })
+
+vim.keymap.set("n", "q", "<cmd>q!<cr>", { silent = true, buffer = new_buf })
+vim.keymap.set("n", "<space>q", "<cmd>q!<cr>", { silent = true, buffer = new_buf })
 
 vim.cmd("normal! G")

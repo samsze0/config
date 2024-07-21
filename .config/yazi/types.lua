6e6fc3ce8da20ad90fad1a7c5685eb8fa883cb1d
Sam Sze <mingsum.sam@gmail.com>
2024-07-21 10:26:45 +0800

    fix yazi types

---
+++ .config/yazi/types.lua
@@ -0,0 +1,13 @@
+---@meta
+
+--- https://luals.github.io/wiki/definition-files/
+--- https://github.com/sxyazi/yazi/issues/657
+
+---@class ya
+ya = {}
+
+---@class Tab
+Tab = {}
+
+---@class ps
+ps = {}
